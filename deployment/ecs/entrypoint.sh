b173bdd0c56ae70ff057ecdfd84d61fb9f49eeeb
yopaz-tuanha <tuanha@yopaz.vn>
2026-01-30 15:41:52 +0700

    update

--- deployment/ecs/entrypoint.sh
+++ deployment/ecs/entrypoint.sh
@@ -12,11 +12,16 @@ fi
 
 echo "USERNAME is set to: $USERNAME"
 
+# Khởi động nginx trước để ALB healthcheck pass
+nginx -g 'daemon off;' &
+NGINX_PID=$!
+
 # Build app với environment variables từ Secrets Manager
 cd /var/www
 export NODE_OPTIONS="--max-old-space-size=1024"
 export VUE_APP_USERNAME="$USERNAME"
 yarn build
 
-# Khởi động nginx
-exec nginx -g 'daemon off;'
+# Đợi build xong, reload nginx
+kill -HUP $NGINX_PID
+wait $NGINX_PID
