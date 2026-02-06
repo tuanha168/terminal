2207055b3d05004e28fa56560fc08bbe2befde2a
yopaz-tuanha <tuanha@yopaz.vn>
2026-02-06 11:19:29 +0700

    update

--- deployment/ecs/entrypoint.sh
+++ deployment/ecs/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Đợi environment variables được inject từ Secrets Manager
-sleep 2
-
 # Kiểm tra USERNAME đã được set
 if [ -z "$USERNAME" ]; then
     echo "Error: USERNAME not found in environment"
@@ -20,7 +17,7 @@ NGINX_PID=$!
 cd /var/www
 export NODE_OPTIONS="--max-old-space-size=1024"
 export VUE_APP_USERNAME="$USERNAME"
-yarn build
+# yarn build
 
 # Đợi build xong, reload nginx
 kill -HUP $NGINX_PID
